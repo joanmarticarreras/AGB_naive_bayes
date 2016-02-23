8636c16628b0dd19ff34bc783932408438fe8b44
Sergio Castillo <s.cast.lara@gmail.com>
2016-02-23 15:52:20 +0100

    Now read more than one file

--- f_calculator.pl
+++ f_calculator.pl
@@ -4,53 +4,73 @@ use strict;
 use Data::Dumper;
 
 
-my $file = shift @ARGV;
+my @files = @ARGV;
 
-my @CANCERS = ("brca", "coad", "hnsc", "kric", "luad", "lusc", "prad", "thca");
-
-open my $fh, "<", $file
-    or die "Can't open $file :$!\n";
-
-my %results = ();
-# Initialize cancers:
-
-foreach my $c (@CANCERS) {
-    $results{$c}->{TP} = 0;
-    $results{$c}->{FP} = 0;
-    $results{$c}->{TN} = 0;
-    $results{$c}->{FN} = 0;
-
-}
-
-while (<$fh>) {
-    chomp;
-    my ($s, $real, $prediction) = split /\t/;
-    foreach my $cancer (@CANCERS) {
-        if ($real eq $cancer) {
-            # TP or FN
-            if ($real eq $prediction) {
-                $results{$cancer}->{TP}++;
-            } else {
-                $results{$cancer}->{FN}++;
-            }
-        } else {
-            # FP or TN
-            if ($prediction eq $cancer) {
-                $results{$cancer}->{FP}++;
-            } else {
-                $results{$cancer}->{TN}++;
-            }
-
-        }
-    }
-}
 
 print "CANCER\tMEASURE\tVALUE\n";
-foreach my $cancer (keys %results) {
-    my $precision = $results{$cancer}->{TP} / ($results{$cancer}->{TP} + $results{$cancer}->{FP});
-    my $recall    = $results{$cancer}->{TP} / ($results{$cancer}->{TP} + $results{$cancer}->{FN});
-    my $f_measure = 2 / ((1/$precision) + (1/$recall));
-    printf ("%s\t%s\t%.4f\n", $cancer, "PRECISION", $precision);
-    printf ("%s\t%s\t%.4f\n", $cancer, "RECALL", $recall);
-    printf ("%s\t%s\t%.4f\n", $cancer, "F", $f_measure);
+foreach my $file (@files) {
+
+    my @CANCERS = ("brca", "coad", "hnsc", "kric", "luad", "lusc", "prad", "thca");
+
+    open my $fh, "<", $file
+        or die "Can't open $file :$!\n";
+
+    # Initialize cancers:
+    my %results = ();
+    foreach my $c (@CANCERS) {
+        $results{$c}->{TP} = 0;
+        $results{$c}->{FP} = 0;
+        $results{$c}->{TN} = 0;
+        $results{$c}->{FN} = 0;
+
+    }
+
+    while (<$fh>) {
+        chomp;
+        my ($s, $real, $prediction) = split /\t/;
+        foreach my $cancer (@CANCERS) {
+            if ($real eq $cancer) {
+                # TP or FN
+                if ($real eq $prediction) {
+                    $results{$cancer}->{TP}++;
+                } else {
+                    $results{$cancer}->{FN}++;
+                }
+            } else {
+                # FP or TN
+                if ($prediction eq $cancer) {
+                    $results{$cancer}->{FP}++;
+                } else {
+                    $results{$cancer}->{TN}++;
+                }
+
+            }
+        }
+    }
+
+    foreach my $cancer (keys %results) {
+        my ($precision, $recall, $f_measure);
+        if (($results{$cancer}->{TP} + $results{$cancer}->{FP}) == 0) {
+            $precision = 0;
+        } else {
+            $precision = $results{$cancer}->{TP} / ($results{$cancer}->{TP} + $results{$cancer}->{FP});
+        }
+        if (($results{$cancer}->{TP} + $results{$cancer}->{FN}) == 0) {
+            $recall = 0;
+        } else {
+            $recall = $results{$cancer}->{TP} / ($results{$cancer}->{TP} + $results{$cancer}->{FN});
+
+        }
+
+        if ($precision == 0 or $recall == 0) {
+            print STDERR "$file\n";
+            $f_measure = 0;
+        } else {
+            $f_measure = 2 / ((1/$precision) + (1/$recall));
+        }
+        printf ("%s\t%s\t%.4f\n", $cancer, "PRECISION", $precision);
+        printf ("%s\t%s\t%.4f\n", $cancer, "RECALL", $recall);
+        printf ("%s\t%s\t%.4f\n", $cancer, "F", $f_measure);
+    }
+
 }
